690458f0fc9e1722ac4fbffd86cebab75ea5ef1a
baoss <18018021075@163.com>
2020-04-23 13:53:11 +0800

    add readme

--- this.js
+++ this.js
@@ -13,7 +13,7 @@ person.fn.call(person1,'hello')
 person.fn.apply(person1, ['hello'])
 person.fn.bind(person1, 'hello')()
 
-// 手动实现call方法
+// 手动实现call方法 apply方法类似 只是参数处理方式不同
 // 1.将调用对象的this指向新对象 因为call和apply作为函数的方法 所以为了执行函数需要在新对象上定义fn
 // 2.处理arguments,从第二个参数开始作为参数列表调用定义的fn
 // 3.删除新增加到新对象上的fn(注意处理对象原本方法名的重复 使用hasOwnProperty及Math.random)
